f7562d10b4528d6965149da937dd58c8aae368b9
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-03-25 17:08:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
 
 client.on('ready', () => {
   client.user.setGame('perder el tiempo .help')
-});
+})
 
 client.on('message', message => {
-    if (message.content === '++que') {
-    	message.reply('so! :cheese: ');
+    if (message.content === '++ping') {
+    	message.channel('**Pong!** :ping_pong:');
   	}
 });
 
