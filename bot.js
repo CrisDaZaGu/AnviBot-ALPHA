1155a4513a5f68ce687136a51e4102a58bcfce12
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-03-25 20:52:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'nuv') {
+    	message.react(':poop:');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
