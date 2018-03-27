8109168d9bfb5e2821a29a6a45f6f089c6792cbd
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-03-26 23:31:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '++cya') {
+    	message.channel.send('Cya, {{username}}, descansa bien y cuídate.');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
