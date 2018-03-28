0edf9bcd7971c6ebb5e4e71a8fa1a65a219157b2
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-03-28 17:25:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setActivity('--help');
+ client.user.setActivity('--help');
 });
 
 client.on('message', message => {
