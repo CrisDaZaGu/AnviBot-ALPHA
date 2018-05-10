a1f480784da7d35f145e6cd03c2f1958208dcfa8
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-05-09 20:36:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "--";
+const prefix = "++";
 
 client.on('ready', () => {
   client.user.setGame(prefix + 'ayuda | AnviBot ALPHA')
