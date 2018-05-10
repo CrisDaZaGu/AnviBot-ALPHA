2980bec6b5e788a7c4f03e6c17a97eb67c310fb6
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-05-09 21:10:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,37 +3,7 @@ const client = new Discord.Client();
 const prefix = "__";
 
 client.on('ready', () => {
-  client.user.setGame('__ayuda | AnviBot Pruebas | anvibot.blogspot.com/alpha')
-});
-
-client.on("message", message => {
-  if (!message.content.startsWith(prefix)) return;
-  
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("**Pong!** :ping_pong:");
-  } else
-  if (message.content.startsWith(prefix + "xDD")) {
-    message.channel.send("xDD");
-  } else
-  if (message.content.startsWith(prefix + "ayuda")) {
-    message.channel.send("Soy un bot too' raro.\n¡Consulta mi lista de comandos con `__comandos`!");
-  } else
-  if (message.content.startsWith(prefix + "comandos")) {
-    message.channel.send("Te envié mis comandos por mensaje privado, " + message.author.username);
-    message.author.send("```Comandos```\n**++ayuda**: Consigue ayuda del bot.\n**++info**: Información del bot\n**++me**: Consigue tu id de jugador.\n**++ping**: ¡Ping Pong! :ping_pong:\n**++avatar**: Consulta la URL de tu avatar actual.\n**++invite**: Invita a AnviBot a tu servidor de Discord!\n**++xDD**: xDD\n```Página```\nhttp://anvibot.blogspot.com/p/commands");
-  } else
-  if (message.content.startsWith(prefix + "info")) {
-    message.channel.send("Estado del Bot: **Conectado**\nFallos: **0**\nCreador: **ElBuenAnvita**\nVersión del Bot: **alpha_0.1.2.9**");
-  } else
-  if (message.content.startsWith(prefix + "invite")) {
-    message.channel.send("```AnviBot Discord Bot ALPHA```\nSi tienes dudas sobre el bot o quieres colaborar, puedes entrar con el siguiente enlace:\nhttps://discord.gg/PSFfWFp\n\n```Invita AnviBot a tu servidor de Discord```\nPuedes invitar al bot con el siguiente enlace:\nhttps://discordapp.com/api/oauth2/authorize?client_id=419980531564806145&permissions=8&scope=bot\n\n**¡Gracias por usar AnviBot n.n!**");
-  }
-});
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix + "me")) {
-      message.channel.send('Usuario: **' + message.author.username + '**\nID:' + message.author.id);
-    }
+  client.user.setGame('5mandos')
 });
 
 client.login(process.env.BOT_TOKEN);
