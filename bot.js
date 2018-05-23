0ec5b4539082798684695d355fe30a73c48c745b
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-05-22 21:00:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,4 +85,18 @@ client.on('message', message => {
     }
 });
 
+client.on("message", async message => {
+  
+  if(message.author.bot) return;
+  
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if(command === "ping") {
+    const m = await message.channel.send("Ok, esperame...")
+    m.edit(`**¡Pong!** :ping_pong: ${m.createdTimestamp - message.createdTimestamp}ms.`)
+  }
+
 client.login(process.env.BOT_TOKEN);
