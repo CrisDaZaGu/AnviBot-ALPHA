2a4d54cdf5acf61c4319cdf53a04063c93ec93d1
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-06-08 18:58:06 -0500

    edit_innecesario

--- bot.js
+++ bot.js
@@ -36,10 +36,10 @@ il.run();
 
 console.log(randomQuote3());
 //from here down is different gif for command --roll ------------------------------------------------------
-const quotes4 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" ]
+const quotes4 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "40", "41" ]
 
 function randomQuote4() {
-	return quotes4[Math.floor(Math.random() * 100)];
+	return quotes4[Math.floor(Math.random() * quotes4.length)];
 };
 il.add(randomQuote4, []);
 
