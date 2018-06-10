25ce3b51df7d33a1818fb2a195e4e5592d1d3a46
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-06-10 10:30:15 -0500

    .

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ client.on("message", message => {
     message.channel.send("**Pong!** :ping_pong:");
   } else
   if (message.content.startsWith(prefix + "info")) {
-    message.channel.send("**Estado del Bot:** Conectado\n**Fallos:** null\n**Creador:** **ElBuenAnvita**\nVersión del Bot: " + version);
+    message.channel.send("**Estado del Bot:** Conectado\n**Fallos:** null\n**Creador:** ElBuenAnvita\n**Versión del Bot:** " + version);
   } else
   if (message.content.startsWith(prefix + "invite")) {
     message.channel.send("```AnviBot Alpha Discord Bot```\nSi tienes dudas sobre el bot o quieres colaborar, puedes entrar con el siguiente enlace:\nhttps://discord.gg/PSFfWFp\n\n```Invita AnviBot a tu servidor de Discord```\nPuedes invitar al bot con el siguiente enlace:\nhttps://discordapp.com/api/oauth2/authorize?client_id=419980531564806145&permissions=8&scope=bot\n\n**¡Gracias por usar AnviBot n.n!**");
