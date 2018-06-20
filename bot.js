a8f0948934f4aaae5964b70b8819b3030670fb6a
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-06-19 19:01:48 -0500

    asasa

--- bot.js
+++ bot.js
@@ -57,6 +57,17 @@ il.add(randomQuote5, []);
 il.run();
 
 console.log(randomQuote5());
+//from here down is different gif for command --roll ------------------------------------------------------
+const quotes6 = ["https://media1.tenor.com/images/ff2dcd44504000e320c21ae5682b5369/tenor.gif?itemid=5749160", "https://pa1.narvii.com/5748/8c6805c5fb2172cfdc445ef193a4527f4492012a_hq.gif", "http://i0.kym-cdn.com/photos/images/original/000/966/850/d79.gif", "https://i.imgur.com/0qCUoL2.jpg", "https://i.imgur.com/0qCUoL2.jpg", "https://media1.tenor.com/images/3918ab9203b15b16cfc872f5540bfedc/tenor.gif?itemid=5958526", "http://i0.kym-cdn.com/photos/images/newsfeed/001/160/891/6d2.gif"]
+
+function randomQuote6() {
+	return Math.floor(Math.random() * quotes6.length);
+};
+il.add(randomQuote6, []);
+
+il.run();
+
+console.log(randomQuote5());
 
 client.on('ready', () => {
   client.user.setGame(prefix + 'ayuda | AnviBot Alpha')
@@ -91,7 +102,7 @@ client.on("message", message => {
   if (message.content.startsWith(prefix + "say")) {
     let text = args.slice(0).join(" ");
     message.channel.send(text);
-  }
+  } else
   if (message.content.startsWith(prefix + "kick")) {
     let text = args.slice(0).join(" ");
     message.channel.send(text);
