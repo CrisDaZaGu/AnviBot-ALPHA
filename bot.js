32699349bc3ce89e500900e6f09655e4d7b3379a
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-09-30 17:55:03 -0500

    Test 2

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ il.add(randomQuote4, []);
 il.run();
 
 //from here down is different QUOTES FOR :VISTO: ------------------------------------------------------
-const quotes8 = [" <:visto:49181876175044608> ", " <:visto:461334819586965507> ", " <:Visto2:482049600602243072> "]
+const quotes8 = ["<:visto:49181876175044608>", "<:visto:461334819586965507>", "<:Visto2:482049600602243072>"]
 
 function randomQuote8() {
 	return quotes8[Math.floor(Math.random() * quotes8.length)];
@@ -49,23 +49,29 @@ il.add(randomQuote8, []);
 il.run();
 
 console.log(randomQuote8());
-//from here down is different gif for command --chiste ------------------------------------------------------
+// Chistes v
 const quotes5 = [
-  "— ¡Soldado López!\n- ¡Sí, mi capitán!\n- ¡No lo ví ayer en la prueba de camuflaje.\n- ¡Gracias, mi capitán",
-  "— ¿Qué pasa si te corto una oreja?\n- Me quedo medio sordo.\n- ¿Y si te corto la otra?\n- Me quedo ciego.\n- ¿Por qué?\n- Porque se me caen los lentes",
+  "—¡Soldado López!\n—¡Sí, mi capitán!\n—¡No lo ví ayer en la prueba de camuflaje!\n—¡Gracias, mi capitán!",
+  "—¿Qué pasa si te corto una oreja?\n—Me quedo medio sordo.\n—¿Y si te corto la otra?\n—Me quedo ciego.\n—¿Por qué?\n—Porque se me caen los lentes",
   "¿Cuál es el colmo de un peluquero?\n¡Perder el bus por los pelos!",
-  "— Ayer ví un letrero que hizo que me cagara en los pantalones\n- ¿Y qué decía?\n - «Baño cerrado»",
-  "Llegan a una casa para el Censo y le preguntan:\n- ¿Su nombre?\n- Adan\n- ¿El de su esposa?\n- Eva\n- ¡Increíble! Por casualidad, ¿La serpiente también vive aquí?\n- Sí, un momento,\n  ¡¡SUEGRAAA, LA BUSCAN!!",
+  "—Ayer ví un letrero que hizo que me cagara en los pantalones\n—¿Y qué decía?\n—«Baño cerrado»",
+  "Llegan a una casa para el Censo y le preguntan:\n—¿Su nombre?\n—Adan\n—¿El de su esposa?\n—Eva\n—¡Increíble! Por casualidad, ¿La serpiente también vive aquí?\n—Sí, un momento,\n  ¡¡SUEGRAAA, LA BUSCAN!!",
   "Cuando muera, pondré en mi tumba «WiFi Gratis»\nAsí vendrá gente a visitarme",
-  "— Señor, su esposa cayó al río\n- Búsquenla río arriba\n- Pero si el río va para abajo\n- Hágame caso, ¡esta pendeja siempre le lleva la contraria a todo!",
-  "Pepito le pregunta a su maestra:\n- Maestra, ¿me castigaría usted por algo que yo no hice?\n- No Pepito, ¡por supuesto que no!\n- Qué bien, porque no hice la tarea de hoy.",
+  "—Señor, su esposa cayó al río\n—Búsquenla río arriba\n—Pero si el río va para abajo\n—Hágame caso, ¡esta pendeja siempre le lleva la contraria a todo!",
+  "Pepito le pregunta a su maestra:\n—Maestra, ¿me castigaría usted por algo que yo no hice?\n—No Pepito, ¡por supuesto que no!\n—Qué bien, porque no hice la tarea de hoy.",
   "Me robaron todas las sillas de mi casa,\nno saben cómo me siento ahora.",
-  "— ¿Nivel de inglés?\n- Alto\n- Traduzca <<Memoria>>\n- Memory\n- Úselo en una oración\n- Una vez me aventé por la ventana y memory\n- ¡CONTRATADO!",
-  "— Oye, ¿por qué te bañas con pintura azul?\n- Es que mi novia vive lejos\n- ¡¿Y eso que tiene que ver?!\n- Es que quiero estar azulado ;-;",
+  "—¿Nivel de inglés?\n—Alto\n—Traduzca <<Memoria>>\n—Memory\n—Úselo en una oración\n—Una vez me aventé por la ventana y memory\n—¡CONTRATADO!",
+  "—Oye, ¿por qué te bañas con pintura azul?\n—Es que mi novia vive lejos\n—¡¿Y eso que tiene que ver?!\n—Es que quiero estar azulado ;-;",
   "—¿A qué te dedicas?\n—Soy rockero\n—Wow, ¿cantas o tocas la guitarra en alguna banda?\n—No, junto rocas y las vendo",
-  "*En una pequeña farmacia del pueblo entra una mujer y dice:*\n—Por favor, quiero comprar Arsénico.\n—No puedo venderle eso. ¿Cuál es su finalidad\n—¡Matar a mi marido!\n—Mucho peor, para ese fin no se lo puedo vender.\n*La mujer abre su cartera y extrae una foto del marido haciendo el amor con la mujer del farmacéutico\n—Ahh, buenooo, con receta es otra cosa...",
+  "*En una pequeña farmacia del pueblo entra una mujer y dice:*\n—Por favor, quiero comprar Arsénico.\n—No puedo venderle eso. ¿Cuál es su finalidad?\n—¡Matar a mi marido!\n—Mucho peor, para ese fin no se lo puedo vender.\n*La mujer abre su cartera y extrae una foto del marido haciendo el amor con la mujer del farmacéutico*\n—Ahh, buenooo, con receta es otra cosa...",
   "—Hijo, si repruebas el examen de mañana olvídate que soy tu padre.\n*Al día siguiente*\n—Hijo, ¿cómo te fue en el exámen?\n—¿Y tú quién eres?",
-  "—Roberto, ¿cuál es tu verdura favorita?\n—La zanahoria\n—¿Me la puedes deletrear?\n—Mmm... ahora que lo pienso es la papa"
+  "—Roberto, ¿cuál es tu verdura favorita?\n—La zanahoria\n—¿Me la puedes deletrear?\n—Mmm... ahora que lo pienso es la papa",
+  "—Mamá, ¿estoy muy peluda?\n—¡Ay, un monito que habla!\n—¡MAMÁAAA!\n—¡AYY, y dice 'mamá'! ¡Me lo quedo!",
+  "¿Qué le dice una impresora a otra?\n*¿Esa hoja es tuya o es impresión mía?",
+  "¿Celosa yo? JAJAJAJAJAJA.\N¿QUIÉN CARAJOS ES REXONA Y POR QUÉ NO TE ABANDONA?",
+  "*Que sea lo que Dios quiera*\nYo entregando el exámen",
+  "—¿Tiene pastillas para la flojera?\n—Sí.\n—¿Me pone una en la boca, por favor?",
+  "Había una vez un niño llamado «Tarea»\nUn día, la profesora dijo «¡Tarea para la casa!»\nY el niño se fue para su casa."
 ]
 
 function randomQuote5() {
@@ -78,8 +84,8 @@ il.run();
 console.log(randomQuote5());
 
 client.on('ready', () => {
-  client.user.setGame('[1] Mantenimimiento');
-  client.user.setStatus('dnd')
+  client.user.setGame(prefix + 'ayuda | AnviBot Beta | [2]');
+  // client.user.setStatus('dnd')
 });
 
 client.on("message", message => {
@@ -122,9 +128,16 @@ client.on('message', message => {
 
 client.on('message', message => {
   if (message.content.startsWith(prefix + "visto")) {
+    const today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    var hh = today.getHours();
+    var min = today.getMinutes();
+    var ss = today.getSeconds();
     const embed = {
       "title": "",
-      "description": randomQuote8(),
+      "description": "Visto " + randomQuote8() + "(" + hh + min + ss + " " + dd + "/" + mm + "/" + yyyy + ")",
       "color": 2335,
     }
     message.channel.send({ embed });
@@ -243,7 +256,7 @@ client.on('message', message => {
       },
       {
         "name": "Comandos modificados",
-        "value": "`visto`: Solo envía un emote random del visto.\n`maps`: Ahora puedes localizar sitios específicos (No solo ciudades)"
+        "value": "`visto`: Solo envía un emote random del visto.\n`maps`: Ahora puedes localizar sitios específicos (No solo ciudades)\n`chistes`: Agregados más malos chistes (y arreglados los errores gramaticales)"
       },
       {
         "name": "Comandos retirados",
