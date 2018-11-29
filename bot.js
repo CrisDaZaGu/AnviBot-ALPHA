ba30cf30115cf8e6e966c3f9ec4c53e0d8881157
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-11-29 15:31:54 -0500

    Conversor Command Test

--- bot.js
+++ bot.js
@@ -269,7 +269,7 @@ client.on('message', async message => {
     let text = "<@!" + message.author.id + ">, aquí tienes los resultados de tu conversión";
     if(!args[1]) return message.reply("no especificaste ningúna moneda. \n**Uso correcto:** `conversor <MONEDA ORIGEN> <MONEDA A CONVERTIR> <Cantidad (sólo número)>\n**Ejemplo:** `conversor EUR USD 5`");
     if(!args[3]) return message.reply("no especificaste la cantidad a convertir.")
-    const res = await got('https://api.cambio.today/v1/quotes/COP/USD/json?quantity=4000&key=290|OztDtH8ycuxHYj9U~_pdMn^0aa_ruSXj', {json: true})
+    const res = await got(`https://api.cambio.today/v1/quotes/COP/USD/json?quantity=${args[3]}&key=290|OztDtH8ycuxHYj9U~_pdMn^0aa_ruSXj`, {json: true})
     message.channel.send('Resultado: ' + res.body.result.amount);
   }
 });
